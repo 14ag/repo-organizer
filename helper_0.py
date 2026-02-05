f08f518eae2d7fee1e1b44317741339745751241
14ag <muriukipn@gmail.com>
2026-02-05 17:41:00 +0300

    completed python todo list

--- helper_0.py
+++ helper_0.py
@@ -1,40 +1,34 @@
 import os
 import sys
 import re #regex
+
 ##############################helper functions########################################
 
 args=sys.argv[1:]
 
-def printSwitch(line0):
-    #returns true to allow main to send out to a new readme
-    regex_start=r"^##\s"
-    regex_end=r"<br>"
-    match_start=re.search(regex_start, line0)
-    match_end=re.search(regex_end, line0)
-    
-    if match_start:
-        return True
-    elif match_end:
-        return False
-    else 
-    
 
-def getBranchName(line1):
-    branch_name=""
-    print_switch=printSwitch(line1)
+def main(str0):
+    line0=str0
 
-    if print_switch:
-        regex1=r"(?<=:).*"
-        branch_name=re.search(regex1,line1).group(0)
-        return branch_name
-    elif not print_switch:
-        return 0
+    def printSwitch():
+        #returns true to allow main to send out to a new readme
+        regex_start=r"^##\s"
+        regex_end=r"<br>"
+        match_start=re.search(regex_start, line0)
+        match_end=re.search(regex_end, line0)
 
+        def getBranchName():
+            regex1=r"(?<=:\s).*"
+            branch_name=re.search(regex1,line0).group(0)
+            return branch_name
+        
+        if match_start:
+            return True, getBranchName()
+        elif match_end:
+            return False, 0
+        else:
+            return True, 0
+        
+    return " ".join(str(i) for i in printSwitch())
 
-
-
-
-
-
-
-os.sys(f'echo {print_switch}')
+os.sys(f'echo {main(args)}')
