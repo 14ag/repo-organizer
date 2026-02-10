9dd52e85cecdaff62a20a5102227abf914fba0b1
14ag <muriukipn@gmail.com>
2026-02-10 23:02:22 +0300

    update .gitignore

--- main.bat
+++ main.bat
@@ -1,39 +1,59 @@
-@echo off
+::@echo off
 setlocal enabledelayedexpansion
-set "py_folder_path="
+::set "repo_path="%userprofile%\sauce\learn-git""
+set "repo_path=learn-git"
+set "py_files_backup=test"
 
-python helper_1.py
+set "readme=readme3.md"
+
+call "cmd /c python helper_1.py"
 
 for /f "delims=" %%a in (readme2.md) do (
     set "line=%%a"
+    echo %%a
     for /f "delims=" %%b in ('python helper_0.py "!line!"') do (
-        ::echo %%b
+        echo %%b
         for /f "tokens=1,* delims= " %%c in ("%%b") do (
-            ::echo %%c_%%d
+            echo %%c_%%d
             if "%%c"=="True" (
                 if not "%%d"=="0" set "branch_name=%%d"
                 echo !line!>>!readme!
             )            
             if "%%c"=="False" (
                 if not "%%d"=="0" (
-                    call main
-                    set "file_name=%%d"
-                )
-            )            
-        )
-    )    
+                    call :main
+                    set "file_name=test\%%d"
+                )   
+            )
+        )   
+    )
+    pause
 )
 
-
 :main
-::gh create branch
-::gh checkout branch
-:: del *.py *.md
-copy !file_name! !repo_folder!!file_name!
-copy !readme! !repo_folder!!readme!
-::gh commit
-::git push
-::
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo branch name _!branch_name!_
+echo python file _!file_name!_
+echo repo folder _!repo_path!_
+copy !file_name! !repo_path!
+copy !readme! !repo_path!
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+exit /b
+
+
+:main0
+pushd "!repo_path!"
+git checkout main
+git checkout -b !branch_name!
+del *.py *.md
+popd
+copy !file_name! !repo_path!
+copy !readme! !repo_path!
+pushd "!repo_path!"
+git add .
+git commit -m "!branch_name!"
+git push
+popd
 
 
 endlocal
--- readme2.md
+++ readme2.md
@@ -1,4 +1,27 @@
 # 22
+
 ## The Challenge: The "Dinner Bill Splitter"
+
+qwertyui defined inputs and outputs.1
+
+qwertyui defined inputs and outputs.2
+
+qwertyui defined inputs and outputs.3
+
+<br>
+<br>
+<br>
+
+# 8
+
+## The Challenge: The "robust calculator"
+
 qwertyui defined inputs and outputs.
+
+qwertyui defined inputs and outputs.2
+
+qwertyui defined inputs and outputs.3
+
+<br>
+<br>
 <br>
